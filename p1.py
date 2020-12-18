ec431b4a4609fed5775ee4db454f2254ede9d3f0
anu-bhav <myster.anubhav@gmail.com>
2020-12-18 20:08:58 +0400

    P.1 Intro and Neuron Code

---
+++ p1.py
@@ -0,0 +1,9 @@
+inputs = [1.2, 4.5, 5.3]
+weights = [2.4, 5.4, 7.6]
+bias = 3
+
+# formula: (inputs x weights) + bias
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+
+print(output)
